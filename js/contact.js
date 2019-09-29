fff0228e527869c604eb2692a5fc3aae4ac90b6d
arturo <artedsolis@gmail.com>
2019-09-29 15:58:25 +0200

    building object and its constructor

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,48 @@
-// objet et prototype  monObjet.maMéthode()
+// objet et constructeur
 
-const contact = {
-  nom: "Lévise",
-  prenom: "Carole"
-}
+var Personaje = {
+  nom: "",
+  prenom: "",
 
-function decrire() {
-  return `${contact.prenom}, ${contact.nom} travaille dans une biblothèque`;
-}
+  // Retorna la descripción del personaje
+  describir: function () {
+    var descripcion = "Nom : " + this.nom + " , Prenom : " +
+      this.vida;
+    return descripcion;
+  },
 
-console.log(decrire(contact);
+};
 
-    var name1 = ("Nom : " + contact.nom + " , " + "Prenom : " + contact.prenom + ", email : ");
+var perso1 = Object.create(Personaje);
+perso1.nom = "Lévisse";
+perso1.prenom = "Carole";
 
-    console.log(name1);
+var perso2 = Object.create(Personaje);
+perso2.nom = "Nelsonne";
+perso2.pre = "Mélodie";
+
+console.log(perso1.describir());
+console.log(perso2.describir());
 
 
+//var name1 = ("Nom : " + contact.nom + " , " + "Prenom : " + contact.prenom + ", email : ");
+
+//console.log(name1);
+
+// tableau d'acces aux options
+const options = ["1 : Lister les contacts", " 2 : Ajouter un contact", " 3 : Quitter"];
+
+console.log("##### Bienvenue dans le gestionaire de contacts ! #####")
 
 
-    // tableau d'acces aux options
-    const options = ["1 : Lister les contacts", " 2 : Ajouter un contact", " 3 : Quitter"];
+options.forEach(option => {
+  console.log(option);
+});
 
-    console.log("##### Bienvenue dans le gestionaire de contacts ! #####")
+const option1 = options[0];
+const option2 = options[1];
+const option3 = options[2];
 
-
-    options.forEach(option => {
-      console.log(option);
-    });
-
-    const option1 = options[0];
-    const option2 = options[1];
-    const option3 = options[2];
-
-    console.log(options[0]); console.log(options[1]); console.log(options[2]);
+console.log(options[0]);
+console.log(options[1]);
+console.log(options[2]);
